d4ad57e90e56ed659ad95c29bd651325b347027e
C1 <yarievakamila@gmail.com>
2022-01-29 19:03:27 +0300

    add  test for TextBox

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,11 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "2048x1153";
+
+    }
+
+}
